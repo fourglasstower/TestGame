13c453a9c15ce726b70062f74806dc55a84ab6da
four <fourglasstower@gmail.com>
2020-07-11 00:16:47 -0500

    1.0.1
    main menu implementation

---
+++ plataformas2Dbase/Assets/mainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour {
+
+    [SerializeField] private float delay = 1f;
+
+    //function to be called on button click
+    public void playGame()
+    {
+        StartCoroutine(LevelLoad());
+    }
+
+    //load level after one sceond delay
+    IEnumerator LevelLoad()
+    {
+
+        float elapsedTime = 0;
+        float currentVolume = AudioListener.volume;
+
+        while (elapsedTime < delay)
+        {
+            elapsedTime += Time.deltaTime;
+            AudioListener.volume = Mathf.Lerp(currentVolume, 0, elapsedTime / delay);
+            yield return null;
+        }
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+ 
+
+    public void quitGame() {
+        Debug.Log("Exiting the game");
+        Application.Quit();
+    }
+
+}
